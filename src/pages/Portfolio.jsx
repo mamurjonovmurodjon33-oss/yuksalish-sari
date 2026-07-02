ab0bcae9504d20ebeb8647597f694ab7a2301ac2
umayr-dev <abuqodir0107@gmail.com>
2026-07-02 19:49:58 +0500

    portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -37,7 +37,112 @@ export default function Portfolio() {
       image: "/images/4book.jpg",
       title: "",
       author: "M.X.JUMAKULOVA",
-      pdfUrl: "USLUBIY QO'LLANMA (3).pdf"
+      pdfUrl: "/USLUBIY QO'LLANMA (3).pdf"
+    },
+    {
+      id: 5,
+      image: "/images/book1.jpg",
+      title: "NamDU Ilmiy Axborotnoma 2024",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/1. NamDU Ilmiy Axborotnoma 2024.pdf"
+    },
+    {
+      id: 6,
+      image: "/images/book1.jpg",
+      title: "O'QUV QO'LLANMA",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/1. O'QUV QO'LLANMA.pdf"
+    },
+    {
+      id: 7,
+      image: "/images/book1.jpg",
+      title: "Qoʻqon DPI ilmiy xabarlari 2025",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/2. Qoʻqon DPI ilmiy xabarlari 2025.pdf"
+    },
+    {
+      id: 8,
+      image: "/images/book1.jpg",
+      title: "USLUBIY-QO'LLANMA",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/2. USLUBIY-QO'LLANMA.pdf"
+    },
+    {
+      id: 9,
+      image: "/images/book1.jpg",
+      title: "Uzmu 2025",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/3. Uzmu 2025.pdf"
+    },
+    {
+      id: 10,
+      image: "/images/book1.jpg",
+      title: "ADPI 2025",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/4. ADPI 2025.pdf"
+    },
+    {
+      id: 11,
+      image: "/images/book1.jpg",
+      title: "Pedagogical System",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/4. Pedagogical System.pdf"
+    },
+    {
+      id: 12,
+      image: "/images/book1.jpg",
+      title: "Motivating students",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/5. Motivating students.pdf"
+    },
+    {
+      id: 13,
+      image: "/images/book1.jpg",
+      title: "Universium 2024",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/5. Universium 2024.pdf"
+    },
+    {
+      id: 14,
+      image: "/images/book1.jpg",
+      title: "IBAST 2025",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/6. IBAST 2025.pdf"
+    },
+    {
+      id: 15,
+      image: "/images/book1.jpg",
+      title: "The Peerian Journal 2025",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/7. The Peerian Journal 2025.pdf"
+    },
+    {
+      id: 16,
+      image: "/images/book1.jpg",
+      title: "Nukus kon 2023",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/8. Nukus kon 2023.pdf"
+    },
+    {
+      id: 17,
+      image: "/images/book1.jpg",
+      title: "ADPI_KONF 2025",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/9. ADPI_KONF 2025.pdf"
+    },
+    {
+      id: 18,
+      image: "/images/book1.jpg",
+      title: "ADTI_konf 2025",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/10. ADTI_konf 2025.pdf"
+    },
+    {
+      id: 19,
+      image: "/images/book1.jpg",
+      title: "Ilm-fan 2025",
+      author: "M.X.JUMAKULOVA",
+      pdfUrl: "/11. Ilm-fan 2025.pdf"
     }
   ];
 
